c72069fd84f0648d9ff43148fc2ffc3972b1d757
Liwncy <52757721+Liwncy@users.noreply.github.com>
2021-09-26 15:19:15 +0800

    Add files via upload
    first commit!

---
+++ onedemo-framework/src/main/java/com/codefishing/onedemo/framework/DemoFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.codefishing.onedemo.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFrameworkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoFrameworkApplication.class, args);
+  }
+
+}
---
+++ onedemo-framework/src/test/java/com/codefishing/onedemo/framework/DemoFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefishing.onedemo.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFrameworkApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
